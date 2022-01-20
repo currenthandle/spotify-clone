5c8800e6175f6fab794b5b6d2dc3aa43907ff044
Casey Siebel <leptone55@gmail.com>
2022-01-19 19:00:32 -0800

    tailwind-scrollbar-hide

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
