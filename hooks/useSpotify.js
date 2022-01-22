085cee1b3dad465fe45f7aabe52406dda3fec39b
Casey Siebel <casey@Caseys-MBP.lan>
2022-01-22 15:26:32 -0800

    Use redirect URI

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -6,6 +6,7 @@ import SpotifyWebApi from "spotify-web-api-node";
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
   clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
 });
 
 function useSpotify() {
